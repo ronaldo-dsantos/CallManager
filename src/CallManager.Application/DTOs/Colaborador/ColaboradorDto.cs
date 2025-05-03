e0eb3510990dd3870747e5f1efc4160aadf65ed8
ronaldo <ronaldo.dsantos@gmail.com>
2025-05-03 00:46:29 -0300

    ajustes das validações no dto de colaborador com data anottations

--- src/CallManager.Application/DTOs/Colaborador/ColaboradorDto.cs
+++ src/CallManager.Application/DTOs/Colaborador/ColaboradorDto.cs
@@ -4,27 +4,28 @@ namespace CallManager.Application.DTOs.Colaborador
 {
     public class ColaboradorDto
     {
-        [Range(1, 999999, ErrorMessage = "A matrícula deve conter entre 1 e 6 dígitos")]
+        [Required(ErrorMessage = "A matrícula do colaborador é obrigatória.")]
+        [Range(1, 999999, ErrorMessage = "A matrícula deve ser um número entre 1 e 999999.")]
         public int Matricula { get; set; }
 
-        [Required(ErrorMessage = "O campo Nome é obrigatório")]
-        [StringLength(100, ErrorMessage = "O Nome deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "O nome do colaborador é obrigatório.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome deve ter entre 2 e 100 caracteres.")]
         public string Nome { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "O campo Gestor é obrigatório")]
-        [StringLength(100, ErrorMessage = "O Gestor deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "O nome do gestor é obrigatório.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome do gestor deve ter entre 2 e 100 caracteres.")]
         public string Gestor { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "O campo Cargo é obrigatório")]
-        [StringLength(100, ErrorMessage = "O Cargo deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "O cargo do colaborador é obrigatório.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O cargo deve ter entre 2 e 100 caracteres.")]
         public string Cargo { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "O campo Setor é obrigatório")]
-        [StringLength(100, ErrorMessage = "A Bandeira deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "O setor do colaborador é obrigatório.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O setor deve ter entre 2 e 50 caracteres.")]
         public string Setor { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "O campo Turno é obrigatório")]
-        [StringLength(100, ErrorMessage = "O Turno deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "O turno do colaborador é obrigatório.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O turno deve ter entre 2 e 50 caracteres.")]
         public string Turno { get; set; } = string.Empty;
     }
 }
